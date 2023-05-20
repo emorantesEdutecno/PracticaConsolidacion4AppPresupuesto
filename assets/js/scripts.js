1a372c8670fd7d6ff6ee6c59b9e25842e86ee0f0
Didier Morantes <emorantes@edutecno.com>
2023-05-20 11:53:02 -0500

    funcionalidad completa añadir gastos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,21 +5,69 @@ function enviarPresupuesto(){
     elParrafoPresupuesto.innerText = valorCajaPresupuesto;
 }
 
-function enviarGasto(){
-    let valorCajaGasto = document.getElementById('txtNombreGasto').value;
-    let valorCantidadGasto = document.getElementById('txtCantidadGasto').value;
-    let valorParrafoPresupuesto = document.getElementById('parrafoPresupuesto').innerText;
-    let elParrafoGasto = document.getElementById('parrafoGasto');
-    let elParrafoSaldo = document.getElementById('parrafoSaldo');
+var arrNombresGastos =[];
+var arrCantidadGastos=[];
+
+function acumularGastos(){
+    let acumuladoGastos=0;
+    // recorre el arreglo de cantidad Gastos y suma su contenido
+    for(let i=0; i<arrCantidadGastos.length;i++){
+        acumuladoGastos = acumuladoGastos + arrCantidadGastos[i];
+    }
+    return acumuladoGastos;
+}
+
+function pintarGastos(){
     let elParrafoNombreGasto = document.getElementById('parrafoNombreGasto');
     let elParrafoValor = document.getElementById('parrafoValor');
 
-    elParrafoNombreGasto.innerText = valorCajaGasto;
-    elParrafoValor.innerText = valorCantidadGasto;
-    elParrafoGasto.innerText = valorCantidadGasto;
+    let unParrafoTextoNuevo = document.createElement('p');
+    let unParrafoValorNuevo = document.createElement('p');
 
-    let elSaldo = parseFloat(valorParrafoPresupuesto) - parseFloat(valorCantidadGasto);
+
+    for(let i=0; i<arrNombresGastos.length; i++){
+        // recorremos el arreglo y extraemos nombre
+        unParrafoTextoNuevo.innerText = arrNombresGastos[i];
+        // recorremos el arreglo y extraemos valor
+        unParrafoValorNuevo.innerText = arrCantidadGastos[i];
+
+        // enviamos a los párrafos correspondientes con append
+        elParrafoNombreGasto.appendChild(unParrafoTextoNuevo);
+        elParrafoValor.appendChild(unParrafoValorNuevo);
+    }
+
+    // elParrafoValor.innerText = valorCantidadGasto;
+}
+
+function enviarGasto(){
+    // variables para alojar los gastos
+    let valorCajaGasto = document.getElementById('txtNombreGasto').value;
+    let valorCantidadGasto = document.getElementById('txtCantidadGasto').value;
+    // almacenamos en los arreglos los gastos añadidos
+    arrNombresGastos.push(valorCajaGasto);
+    arrCantidadGastos.push(parseFloat(valorCantidadGasto));
+
+    console.log('arreglo gastos:');
+    console.log(arrNombresGastos);
+    console.log('cantidad gastos:');
+    console.log(arrCantidadGastos);
+ 
+
+    let elAcumuladoGastos =  acumularGastos(); 
+    console.log(elAcumuladoGastos);
+    
+    let valorParrafoPresupuesto = document.getElementById('parrafoPresupuesto').innerText;
+    let elParrafoGasto = document.getElementById('parrafoGasto');
+    let elParrafoSaldo = document.getElementById('parrafoSaldo');
+
+
+    // enviamos el acumlado del gasto al parrafo gasto
+    elParrafoGasto.innerText = elAcumuladoGastos;
+
+    let elSaldo = parseFloat(valorParrafoPresupuesto) - parseFloat(elAcumuladoGastos);
     elParrafoSaldo.innerText = elSaldo;
+
+    pintarGastos();
 
 
 }
